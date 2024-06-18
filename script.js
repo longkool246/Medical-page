26ce40e380c534473618f0f02b4e0f17372bad3b
Nguyen Hai Long <longkool246@gmail.com>
2024-06-18 10:30:50 +0700

    language selector

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function autoShowSlides() {
 let autoSlideInterval = setInterval(autoShowSlides, 3000);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
   // Reset lại khoảng thời gian khi người dùng nhấn nút
   clearInterval(autoSlideInterval);
   autoSlideInterval = setInterval(autoShowSlides, 3000);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
   // Reset time
   clearInterval(autoSlideInterval);
   autoSlideInterval = setInterval(autoShowSlides, 3000);
@@ -27,14 +27,18 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
